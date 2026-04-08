92bd80f2fa2fda068b5e60524f817ecab2147b87
rezoan111 <rezoan111@gmail.com>
2026-04-08 00:31:54 -0400

    music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,23 @@
+-- Part I: Music database
+-- This file creates tables for artists, albums, and songs.
 
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY ASC,
+    name TEXT
+);
+
+CREATE TABLE albums (
+    id INTEGER PRIMARY KEY ASC,
+    name TEXT,
+    artist_id INTEGER,
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY ASC,
+    name TEXT,
+    album_id INTEGER,
+    track_number INTEGER,
+    length_seconds INTEGER,
+    FOREIGN KEY (album_id) REFERENCES albums(id)
+);
